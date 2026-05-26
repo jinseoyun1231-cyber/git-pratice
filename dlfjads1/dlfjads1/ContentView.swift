2b984cacbdab861367ace91ea6423ec6190da5e2
Sally <jin.seoyun1231@gmail.com>
2026-05-26 17:10:47 +0900

    ㄴㅁ읾ㅇ

--- dlfjads1/dlfjads1/ContentView.swift
+++ dlfjads1/dlfjads1/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Bye, world!")
+            Text("에벱베")
         }
         .padding()
     }
