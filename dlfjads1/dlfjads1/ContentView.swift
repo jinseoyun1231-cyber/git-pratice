31212903a349f4badbc47c89f5ac54232a3cdac0
Sally <jin.seoyun1231@gmail.com>
2026-05-26 17:05:22 +0900

    bye world 추가

---
+++ dlfjads1/dlfjads1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  dlfjads1
+//
+//  Created by Seoyun Jin on 5/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Bye, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dlfjads1/dlfjads1/dlfjads1App.swift
@@ -0,0 +1,17 @@
+//
+//  dlfjads1App.swift
+//  dlfjads1
+//
+//  Created by Seoyun Jin on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct dlfjads1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
